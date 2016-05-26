43ac54c179496b2876420eaae8155df167ff8cd8
Douglas Anderson <douglas.j.anderson@gmail.com>
2016-05-26 12:54:34 -0600

    c++ style headers put c lib fns in std namespace

--- gr-ltetrigger/lib/mib_impl.cc
+++ gr-ltetrigger/lib/mib_impl.cc
@@ -232,7 +232,7 @@ namespace gr {
 
       d = pmt::dict_add(d,
                         pmt::intern("tracking_start_time"),
-                        pmt::from_long(time(0)));
+                        pmt::from_long(std::time(0)));
 
       return d;
     }
